aadb16567ab5b7e0ca061046c2175e6e61674481
SayYoungMan <nickte89@gmail.com>
2021-07-20 16:13:40 +0100

    (feat): Grid, Thumbnails and Spinner added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 // Components
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -26,6 +29,21 @@ const Home = () => {
           text={state.results[0].overview}
         />
       ) : null}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
+      </Grid>
+      <Spinner />
     </>
   );
 };
